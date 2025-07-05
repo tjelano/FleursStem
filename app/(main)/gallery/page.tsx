57e89a632e31cc0023926b024b535a03fb5e3356
tjelano <tjelano1@live.nl>
2025-07-05 21:15:53 +0200

    Enable SSR for /gallery page so changes in Sanity Studio are always reflected immediately

--- app/(main)/gallery/page.tsx
+++ app/(main)/gallery/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { client } from "@/sanity/lib/client";
 import { GALLERIES_QUERY } from "@/sanity/queries/gallery-document";
 import { urlFor } from "@/sanity/lib/image";
